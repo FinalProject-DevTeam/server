46e9f058cbef2a71c0b428576de2368d64e85640
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-08-23 21:53:58 +0700

    finished download ofprediction csv file and conversion to array

--- controllers/aws.js
+++ controllers/aws.js
@@ -1,6 +1,8 @@
 const AWS = require('aws-sdk');
 const fs = require('fs');
-const unzip = require('unzip');
+const zlib = require('zlib');
+const csv = require('csv');
+let parse = csv.parse;
 
 const customersDataSchema = require('../aws/schemas/customersDataSchema.json');
 const transactionsDataSchema = require('../aws/schemas/transactionsDataSchema.json');
@@ -22,8 +24,41 @@ class awsController {
   static async getPrediction(req, res) {
     let params = {
       Bucket: 'aws-ml-tutorial-final-project-explore',
-      Key: ''
+      Key: 'batch-prediction/result/bp-5NhckrSnBw0-customers.csv.gz'
     }
+    s3.getObject(params, function (err, data) {
+      if (err) {
+        res
+          .status(400)
+          .json(err);
+        console.log(err);
+      } else {
+        zlib.unzip(data.Body, (err, buffer) => {
+          if (!err) {
+            let csvString = buffer.toString();
+            parse(csvString, function(err, output) {
+              let processedArr = [output[0]]
+              for (let i = 1; i < output.length; i++) {
+                let processedRow = [];
+                let currentRow = output[i];
+                for (var j = 0; j < currentRow.length; j++) {
+                  let processedDatum = Number(currentRow[j].slice(0, 1));
+                  processedRow.push(processedDatum);
+                }
+                processedArr.push(processedRow);
+              }
+              res
+                .status(200)
+                .json(processedArr);
+            });
+          } else {
+            res
+              .status(400)
+              .json(err);
+          }
+        })
+      }
+    });
   }
 }
 
