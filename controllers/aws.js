4392ab192a8efaadaee7db0b49d8387e65003bb8
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-08-23 17:03:08 +0700

    added path at AWS.config

--- controllers/aws.js
+++ controllers/aws.js
@@ -1,11 +1,10 @@
 const AWS = require('aws-sdk');
 const fs = require('fs');
 
-const awsConfig = require('../awsConfig.json');
 const customersDataSchema = require('../aws/schemas/customersDataSchema.json');
-const transactionsDataSchema.json = require('../aws/schemas/transactionsDataSchema.json');
+const transactionsDataSchema = require('../aws/schemas/transactionsDataSchema.json');
 
-AWS.config.loadFromPath(awsConfig);
+AWS.config.loadFromPath('./awsConfig.json');
 
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 const machinelearning = new AWS.MachineLearning({apiVersion: '2014-12-12'});
